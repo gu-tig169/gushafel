fe45690f3c70371f42aa3fe7f0d2b474265c9bc2
Ellen Hafström <hafstrom.ellen@gmail.com>
2020-11-27 17:35:26 +0100

    Steg3.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'homepage.dart';
 
 void main() {
   var state = MyState();
+  state.getList();
 
   runApp(
     ChangeNotifierProvider(create: (context) => state, child: ToDoApp()),
--- lib/taskmodel.dart
+++ lib/taskmodel.dart
@@ -1,31 +1,64 @@
 import 'package:flutter/material.dart';
+import 'getApi.dart';
 
 class Todos {
   String input;
+  String id;
   bool done;
 
   Todos({
     this.input,
+    this.id,
     this.done = false,
   });
+
+  static Map<String, dynamic> toJson(Todos todo) {
+    return {
+      'input': todo.input,
+      'done': todo.done,
+    };
+  }
+
+  static Todos fromJson(Map<String, dynamic> json) {
+    return Todos(
+      input: json['title'],
+      id: json['id'],
+      done: json['done'],
+    );
+  }
 }
 
 class MyState extends ChangeNotifier {
   List<Todos> _list = [];
   List<Todos> get list => _list;
 
-  void addTodo(Todos todo) {
-    _list.add(todo);
+  String _filterTodo = 'all';
+  String get filterTodo => _filterTodo;
+
+  Future getList() async {
+    List<Todos> list = await Api.getTodos();
+    _list = list;
     notifyListeners();
   }
 
-  void removeTodo(Todos todo) {
-    _list.remove(todo);
-    notifyListeners();
+  void addTodo(Todos todo) async {
+    await Api.addTodo(todo);
+    await getList();
   }
 
-  void checkTodo(Todos todo, bool done) {
+  void removeTodo(Todos todo) async {
+    await Api.removeTodo(todo.id);
+    await getList();
+  }
+
+  void checkTodo(Todos todo, bool done) async {
     todo.done = done;
+    await Api.refreshTodo(todo, todo.id);
+    await getList();
+  }
+
+  void setFilterTodo(String filterToDo) async {
+    this._filterTodo = filterTodo;
     notifyListeners();
   }
 }
